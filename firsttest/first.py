87ad5b576cd02c2e5653e92534a37cc4d9d05a45
infcnwangjie <wangjie_rj@163.com>
2019-09-16 19:54:58 +0800

    d

--- firsttest/first.py
+++ firsttest/first.py
@@ -1,13 +1,15 @@
 #coding=utf-8
 import cv2 as cv
-#img = cv.imread("/root/test.jpg")
-
-#cv.namedWindow("Image")
-#cv.imshow("Image",img)
-#cv.waitKey(0)
-#cv2.destroyAllWindows() 
 src=cv.imread('Hydrangeas.jpg')
-cv.namedWindow('input image',cv.WINDOW_AUTOSIZE)  
-cv.imshow('input image',src)  
+# print(src[0,0])
+# print(src.item(0,0,0))
+# src.itemset((0,0,0),50)
+# print(src.item(0,0,0))
+
+# srcroi=src[100:200,100:200]
+# src[200:300,200:300]=srcroi
+cv.namedWindow('input image',cv.WINDOW_AUTOSIZE)
+cv.imshow('input image',src)
+cv.waitKey(0)
 
 
