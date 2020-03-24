a48c5ec089edbc134dd7bcb608d285a41198fd86
infcnwangjie <wangjie_rj@163.com>
2020-03-24 08:12:24 +0800

    opencv

--- Test/main.py
+++ Test/main.py
@@ -4,6 +4,6 @@ import cv2
 from core.target_detect.pointlocation import PointLocationService, BAG_AND_LANDMARK
 
 if __name__ == '__main__':
-	im = cv2.imread('C:/work/imgs/test/bag5.bmp')
+	im = cv2.imread('C:/work/imgs/test/bag6.bmp')
 	a = PointLocationService(img=im)
 	a.location_objects(flag=BAG_AND_LANDMARK)
